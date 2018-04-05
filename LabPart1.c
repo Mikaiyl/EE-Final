dcc1f829819d82db6c655205d4c80d163808326b
Mikaiyl <mikaiyl.davis@icloud.com>
2018-04-05 09:29:45 -0400

    Indentation

--- LabPart1.c
+++ LabPart1.c
@@ -7,24 +7,24 @@
 
 
 /***********************
-*  Global Variables    *
-************************/
+ *  Global Variables    *
+ ************************/
 long int absoluteTime=0;
 int bogusISRFlag= 0;
 
 /***********************
-*  Interrupting cows   *
-************************/
+ *  Interrupting cows   *
+ ************************/
 
 interrupt void RTI_ISR(void) {
-      absoluteTime++;  
-      CRGFLG = 0x80;
-  }
+    absoluteTime++;  
+        CRGFLG = 0x80;
+}
 
 interrupt void UnimplementedISR(void) 
-  {  // handle bogus interrupts
-      bogusISRFlag=1; 
-  }
+{  // handle bogus interrupts
+    bogusISRFlag=1; 
+}
 
 typedef void (*near tIsrFunc)(void); // tIsrFunc is a pointer to a function
 const tIsrFunc _vect[] @0x3E70 = 
@@ -38,53 +38,53 @@ const tIsrFunc _vect[] @0x3E70 =
         UnimplementedISR,    /* vector 02: COP */
         UnimplementedISR,    /* vector 01: Clock monitor fail */
         UnimplementedISR		 /* Reset vector */
- };
+};
 
 /***********************
-*  Function Prototypes *
-************************/
+ *  Function Prototypes *
+ ************************/
 void initAll(void); 
 uint scaleV(uint input);
 int freak(char i);
 
 /***********************
-*  We arrive in Maine  *
-************************/
+ *  We arrive in Maine  *
+ ************************/
 
 void main(){
     int checkHigh, checkLow; // checking for highs
     char chump[33];          // buffer char
     initAll();
-    while(1){
-        if(ATD0STAT0);      // wait for conversion
-        (void)sprintf(chump,"%4d  mV \n P%d    F%d", scaleV((uint)ATD0DR2),checkLow,freak(ATD0DR2));
-        LCDshark(chump);
-        
-        if(checkHigh < ATD0DR2) checkHigh = ATD0DR2 ;
-        if(checkLow > ATD0DR2) checkLow = ATD0DR2;
-    }
-
-}
-
-/***********************
-*  Function definitions*
-************************/
-int freak(char i){ // this is a freaky frequency
-		int r;
-		r = 255000 / i;
-	  return r;
-	}
-void initAll(void){
-    ATDinit();
-    LCDinit();
-    RTIInit(0x13);
+        while(1){
+            if(ATD0STAT0);      // wait for conversion
+            (void)sprintf(chump,"%4d  mV \n P%d    F%d", scaleV((uint)ATD0DR2),checkLow,freak(ATD0DR2));
+                LCDshark(chump);
+                
+                if(checkHigh < ATD0DR2) checkHigh = ATD0DR2 ;
+                    if(checkLow > ATD0DR2) checkLow = ATD0DR2;
+        }
     
 }
 
+/***********************
+ *  Function definitions*
+ ************************/
+int freak(char i){ // this is a freaky frequency
+    int r;
+        r = 255000 / i;
+        return r;
+}
+void initAll(void){
+    ATDinit();
+        LCDinit();
+        RTIInit(0x13);
+        
+}
+
 uint scaleV(uint input){
-     ulong limitH = 4300 ,limitL = 0;
-     ulong max = 25300 ,min = 0;
-     return (((input - min) * limitH) / (max - min)) + limitL;
+    ulong limitH = 4300 ,limitL = 0;
+        ulong max = 25300 ,min = 0;
+        return (((input - min) * limitH) / (max - min)) + limitL;
 } 
 
 
